b53e32a9f83e8ec4b5cd71ce15c6da0e095670bb
Daniel Lucas <hi@danlgz.io>
2024-11-03 14:34:59 -0600

    feat: list beers stock use case (initial version)

---
+++ api/application/use_cases/list_beers_stock_by_ids.py
@@ -0,0 +1,18 @@
+import logging
+from typing import List
+
+from domain.entities import BeerStock
+from domain.repositories import BeersStockRepository
+
+logger = logging.getLogger(__name__)
+
+
+class ListBeersStockByIds:
+    def __init__(self, beers_stock_repository: BeersStockRepository):
+        self._beers_stock_repository = beers_stock_repository
+
+    def execute(self, ids: List[str]) -> List[BeerStock]:
+        results = self._beers_stock_repository.list_by_ids(ids=ids)
+
+        logger.info("list beers stock done!", len(results))
+        return results
