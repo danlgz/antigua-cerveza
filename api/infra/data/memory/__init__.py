c4d03fc2ed6213b99bcdb63f6bb00c464c6571e9
Daniel Lucas <hi@danlgz.io>
2024-11-03 15:57:53 -0600

    feat: order repository (initial version)

--- api/infra/data/memory/__init__.py
+++ api/infra/data/memory/__init__.py
@@ -1,3 +1,4 @@
 from .beers_stock import MemoryBeersStockRepository
+from .orders import MemoryOrdersRepository
 
-__all__ = ["MemoryBeersStockRepository"]
+__all__ = ["MemoryBeersStockRepository", "MemoryOrdersRepository"]
