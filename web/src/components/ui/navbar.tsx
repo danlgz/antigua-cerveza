5642285a9e8f7b6358a3d463acd69b0114bdf0e5
Daniel Lucas <hi@danlgz.io>
2024-11-04 12:47:32 -0600

    chore: logo redirect

--- web/src/components/ui/navbar.tsx
+++ web/src/components/ui/navbar.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 
 type Props = {
   lightLogo: StaticImageData,
@@ -8,8 +9,10 @@ type Props = {
 export default function Navbar({ lightLogo, darkLogo }: Props) {
   return (
     <header className="flex justify-center border-b py-4 shadow-sm sticky top-0 bg-background z-10">
-      <Image src={darkLogo} className="hidden dark:block" alt="Antigua Cerveza logo" width={200} height={50} />
-      <Image src={lightLogo} className="block dark:hidden" alt="Antigua Cerveza logo" width={200} height={50} />
+      <Link href="/">
+        <Image src={darkLogo} className="hidden dark:block" alt="Antigua Cerveza logo" width={200} height={50} />
+        <Image src={lightLogo} className="block dark:hidden" alt="Antigua Cerveza logo" width={200} height={50} />
+      </Link>
     </header>
   )
 }
