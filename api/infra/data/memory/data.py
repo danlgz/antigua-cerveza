6e89bbf9d8d07b5b660002849a8c16173c536eda
Daniel Lucas <hi@danlgz.io>
2024-11-04 08:56:02 -0600

    chore: more mock data

--- api/infra/data/memory/data.py
+++ api/infra/data/memory/data.py
@@ -31,7 +31,7 @@ BEERS_STOCK = [
         img="muy-noble",
     ),
     BeerStock(
-        id=UUID("4cc1aad9-6800-4161-a7d5-83564cfad766"),
+        id=UUID("ae32e245-b08d-434f-b4c8-aef38aef7e38"),
         name="Don Nadie",
         price=5.18,
         last_updated=datetime.strptime("2024-10-03 15:30:00", date_format),
@@ -45,7 +45,7 @@ ORDERS = [
         id=UUID("96413732-3142-47c4-ad78-e99fec8a5492"),
         discounts=4.0,
         items=[],
-        paid=False,
+        paid=True,
         rounds=[
             OrderRound(
                 created=datetime.strptime("2024-10-03 15:30:00", date_format),
@@ -97,5 +97,78 @@ ORDERS = [
         subtotal=0,
         taxes=4.0,
         total=0,
-    )
+    ),
+    Order(
+        id=UUID("8fa3f53a-0319-4df7-bc3a-1968cb21b191"),
+        discounts=4.0,
+        items=[],
+        paid=True,
+        rounds=[
+            OrderRound(
+                created=datetime.strptime("2024-10-03 15:50:00", date_format),
+                items=[
+                    OrderRoundItem(
+                        beer_stock_id=UUID("6aa57346-1357-47b0-923e-7a416427afae"),
+                        quantity=6
+                    ),
+                ]
+            )
+        ],
+        subtotal=0,
+        taxes=4.0,
+        total=0,
+    ),
+    Order(
+        id=UUID("0ce1d1f7-9f09-436a-955f-0760bb4e8a46"),
+        discounts=4.0,
+        items=[],
+        paid=True,
+        rounds=[
+            OrderRound(
+                created=datetime.strptime("2024-10-03 15:30:00", date_format),
+                items=[
+                    OrderRoundItem(
+                        beer_stock_id=UUID("6aa57346-1357-47b0-923e-7a416427afae"),
+                        quantity=2
+                    )
+                ]
+            ),
+            OrderRound(
+                created=datetime.strptime("2024-10-03 15:50:00", date_format),
+                items=[
+                    OrderRoundItem(
+                        beer_stock_id=UUID("ae32e245-b08d-434f-b4c8-aef38aef7e38"),
+                        quantity=3
+                    ),
+                    OrderRoundItem(
+                        beer_stock_id=UUID("6aa57346-1357-47b0-923e-7a416427afae"),
+                        quantity=6
+                    )
+                ]
+            )
+        ],
+        subtotal=0, # calculated from rounds
+        taxes=10, # this should be calculated based on some tax country criteria, in this example will be hardcoded
+        total=0, # calculaded from subtotal, taxes and discounts
+    ),
+    Order(
+        id=UUID("335fa884-1cda-4a3d-b5b5-2381b6928de2"),
+        discounts=0.0,
+        items=[],
+        paid=True,
+        rounds=[
+            OrderRound(
+                created=datetime.strptime("2024-10-03 15:50:00", date_format),
+                items=[
+                    OrderRoundItem(
+                        beer_stock_id=UUID("152b6a83-2069-4cd8-a67e-9eb70dfc444b"),
+                        quantity=10
+                    ),
+                ]
+            )
+        ],
+        subtotal=0,
+        taxes=4.0,
+        total=0,
+    ),
 ]
